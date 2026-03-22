bc1d1ef335d017f76e188a43be2e63f2ab399aad
Aidan Hughes <a.jameshughes11@gmail.com>
2026-03-22 22:52:48 +0000

    Moving files, added a pyproject.toml to ease use on different devices and made readme to explain initialisation

--- code/src/LWR_Model/__init__.py
+++
@@ -1,3 +0,0 @@
-from distances import walk_lat_long, distance_lat_long
-
-__all__ = ["walk_lat_long", "distance_lat_long"]
---
+++ code/src/lwr_model/__init__.py
@@ -0,0 +1,3 @@
+from .distances import walk_lat_long, distance_lat_long
+
+__all__ = ["walk_lat_long", "distance_lat_long"]
